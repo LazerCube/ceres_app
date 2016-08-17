aa10623e9165d477e12a68bb7ab57c665c3cdfba
Elliot <elliot@lazercube.com>
2016-08-17 17:17:57 +0100

    forms magically work

--- src/app/components/pages/public/login-page/login-page.ts
+++ src/app/components/pages/public/login-page/login-page.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { ControlGroup, Control, FormBuilder, Validators, FORM_DIRECTIVES } from '@angular/common';
 import { Router, RouterLink } from '@angular/router-deprecated';
 
 import { AuthService } from '../../../../services/auth.service';
@@ -6,37 +7,77 @@ import { EventsService } from '../../../../services/events.service';
 
 @Component({
     selector: 'login-page',
-    directives: [RouterLink],
-    providers: [],
+    directives: [RouterLink, FORM_DIRECTIVES],
     templateUrl: './app/components/pages/public/login-page/login-page.html',
 })
 
+// export class Login {
+//     public errorMsg:string = '';
+//
+//     constructor(
+//         private authService: AuthService,
+//         public router: Router,
+//         private eventsService: EventsService) {}
+//
+//     login(event, username, password) {
+//         event.preventDefault();
+//         this.authService.login(username, password)
+//             .subscribe(
+//                  data => {
+//                      localStorage.setItem("id_token", data.json().access_token);
+//                  },
+//                  err => console.log(err.text()),
+//                  () => {
+//                      console.log("Authorizated");
+//                      this.eventsService.showNavigation.emit(true);
+//                      this.router.parent.navigate(['Home-Page']);
+//                  }
+//             );
+//     }
+//
+//     signup(event) {
+//         event.preventDefault();
+//         this.router.parent.navigateByUrl('/signup');
+//     }
+// }
+
+// export class Login {
+//     private loginForm: any;
+//
+//     constructor(_builder: FormBuilder) {
+//         this.loginForm = _builder.group({
+//             username: ["", Validators.required],
+//             password: ["", Validators.required]
+//         });
+//     }
+//
+//     doLogin(event) {
+//         if (this.loginForm.dirty && this.loginForm.valid) {
+//             console.log(this.loginForm.value);
+//             event.preventDefault();
+//         }
+//     }
+// }
+
 export class Login {
-    public errorMsg:string = '';
+    loginForm: ControlGroup;
 
-    constructor(
-        private authService: AuthService,
-        public router: Router,
-        private eventsService: EventsService) {}
+    username: Control;
+    email: Control;
 
-    login(event, username, password) {
-        event.preventDefault();
-        this.authService.login(username, password)
-            .subscribe(
-                 data => {
-                     localStorage.setItem("id_token", data.json().access_token);
-                 },
-                 err => console.log(err.text()),
-                 () => {
-                     console.log("Authorizated");
-                     this.eventsService.showNavigation.emit(true);
-                     this.router.parent.navigate(['Home-Page']);
-                 }
-            );
+    constructor(private _builder: FormBuilder) {
+
+        this.username = new Control("", Validators.required);
+
+        this.loginForm = _builder.group({
+            username:  this.username
+        });
     }
 
-    signup(event) {
-        event.preventDefault();
-        this.router.parent.navigateByUrl('/signup');
-    }
+    doLogin(event) {
+       if (this.loginForm.dirty && this.loginForm.valid) {
+           console.log(this.loginForm.value);
+           event.preventDefault();
+       }
+   }
 }
